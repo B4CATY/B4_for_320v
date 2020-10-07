066965a5963f4712daabcce4a69a7a74d028a849
B4CATY <72354577+B4CATY@users.noreply.github.com>
2020-10-07 20:25:38 +0300

    Add files via upload

---
+++ Lab1 b4/Lab 1.2/Lab1.2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+using namespace std;
+
+
+void main()
+{
+    setlocale(LC_ALL, "ru");
+
+	
+	double a, b, c, S, p;
+
+	
+	
+	printf("Введите 1-ую сторону треугольника ABC: ", "\n");
+	scanf_s("%lf", &a);
+	
+	printf("Введите 2-ую сторону треугольника ABC: ", "\n");
+	scanf_s("%lf", &b);
+	
+	printf("Введите 3-ую сторону треугольника ABC: ", "\n");
+	scanf_s("%lf", &c);
+	
+	p = (a + b + c) / 2;
+	printf("Полупериметр равен: %lf \n", p, "\n");
+	
+	S = sqrt(p * (p - a) * (p - b) * (p - c));
+	printf("Площадь равна: %lf", S, "\n");
+
+
+
+}
+
+
+
---
+++ Lab1 b4/Lab 1.4/Lab 1.4.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+
+void main()
+{
+	setlocale(LC_ALL, "ru");
+
+
+	printf("Первый случай\n");
+	int a = 17;
+	int b = 81;
+
+	printf("Было сначало: %d, %d ", a, b, "\n");
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	printf("Стало: %d, %d ", a, b, "\n");
+
+	printf("Второй случай\n");
+	int c, d, f;
+
+	c = 22;
+	d = 15;
+	printf("Было сначало: %d, %d ", c, d, "\n");
+
+	f = c;
+	c = d;
+	d = f;
+	printf("Стало: %d, %d ", c, d, "\n");
+
+}
+
