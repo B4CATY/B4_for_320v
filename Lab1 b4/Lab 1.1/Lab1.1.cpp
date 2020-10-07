b294e2e809ac4f251aad1c52990cc32df215c955
B4CATY <72354577+B4CATY@users.noreply.github.com>
2020-10-07 19:26:43 +0300

    Add files via upload

---
+++ Lab1 b4/Lab 1.1/Lab1.1.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <iostream>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "ru");
+
+
+	printf("Размер в байтах int : %d \n", sizeof(int));
+	printf("Размер в байтах short: %d \n", sizeof(short));
+	printf("Размер в байтах lond : %d \n", sizeof(long));
+	printf("Размер в байтах char : %d \n", sizeof(char));
+	printf("Размер в байтах float : %d \n", sizeof(float));
+	printf("Размер в байтах double : %d", sizeof(double));
+	printf("\n\n\n");
+
+}
+
---
+++ Lab1 b4/Lab 1.6/Lab 1.6.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "ru");
+	double sec, minutes, hours, days, years, full;
+	sec = 60;
+	minutes = 60;
+	hours = 24;
+	days = 365;
+
+	years = 1; // первое условие
+	full = sec * minutes * hours * days * years;
+	printf("В одном году секунд: %lf \n", full);
+
+	years = 100; //второе условие
+	full = sec * minutes * hours * days * years;
+	printf("В 100 годах секунд: %lf \n", full);
+
+	years = 1000; //третье условие
+	full = sec * minutes * hours * days * years;
+	printf("В 1000 годах секунд: %lf", full);
+}
