e2f9082a038b0c353afab87df875297e01d7cfe9
B4CATY <72354577+B4CATY@users.noreply.github.com>
2020-10-12 14:13:06 +0300

    Add files via upload

---
+++ Lab1_b4/Lab1.3/Lab1.3.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <iostream>
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	float x, y, z, n;
+
+	printf("Введите число: ");
+	scanf_s("%f", &x);
+
+	y = x * x;
+	z = y * y;
+	n = z * z;
+
+	printf("Число в 8 степени равно: %f", n);
+}
---
+++ Lab1_b4/Lab1.5/Lab1.5.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	int a, b;
+
+	printf("Введите любое число: ");
+	scanf_s("%d", &a);
+
+	b = a + 100;
+	printf("\nВы ввели число: %d,\n Но кокомпютер выиграл,\n Так как ввел большее число - %d", a, b);
+}
+
